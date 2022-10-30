1b019894882884e8a5a732d5c336f2b189b25d24
dmNetTutorials <95587345+dmNetTutorials@users.noreply.github.com>
2022-10-30 23:20:49 +0200

    some changes (#2)

---
+++ IOSamples/CPlusPlusIOSample/Login/Main.cpp
@@ -0,0 +1,78 @@
+#include"Necessary_Headers.h"
+#include<iostream>
+
+using namespace std;
+
+int startupAuthorization(int argumentsCount,
+	char* argumentsValues[])
+{
+	// кількість символів в паролі
+	int passwordLength = 0;
+
+	// дані передані в програму під час запуску
+	for (int argumentIndex = 1;
+		argumentIndex < argumentsCount;
+		argumentIndex++)
+	{
+		// назва параметра
+		const char* argumentKey = argumentsValues[argumentIndex];
+		// значення параметра
+		const char* argumentValue = argumentsValues[argumentIndex + 1];
+
+		// для порівняння рядків, в С++ використовуєтсья
+		// функція strcmp, у випадку коли обидва рядки 
+		// однакові то результатом функції буде нуль 
+		if (strcmp(argumentKey, "-login") == 0)
+		{
+			// якщо користувач вказав login
+			cout << "Ви авторизувались як\t" << argumentValue << endl;
+		}
+		else
+		{
+			if (strcmp(argumentKey, "-password") == 0)
+			{
+				// якщо користувач вказав пароль
+				passwordLength = strlen(argumentValue);
+				// припинити виконання циклу
+				break;
+			}
+		}
+
+	}
+
+	return passwordLength;
+
+}
+
+// Головна функція 
+int main(int argumentsCount,		 	// кількість параметрів, переданих під час запуску
+		 char* argumentsValues[],		// значення переданих параметрів 
+		 char* environmentParameters[]  // змінні оточення, для поточного користувача
+)
+{
+
+	// підтримка поточної локалізаці
+	// (мови та національних особливостей, 
+	// таких як валюта, формат часу і т.д.)
+	//setlocale(LC_ALL, "");
+
+	// якщо кількість переданих параметрів більше 1
+	// 1-й параметр завжди є назвою програми, що запускається
+	if (argumentsCount > 1)
+	{
+		return startupAuthorization(argumentsCount, argumentsValues);
+	}
+	else
+	{
+		// дані передаються в програму під час виконання
+		// menu();
+	}
+
+	// зачекати поки користувач 
+	// натисне будь-яку клавішу, щоб 
+	// закрити програму
+	cin.get();
+
+	return 0;
+
+}
---
+++ IOSamples/CPlusPlusIOSample/Login/Necessary_Headers.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// Οεπελ³κ βρ³υ νεξαυ³δνθυ τΰιλ³β
+
+#include<iostream>
