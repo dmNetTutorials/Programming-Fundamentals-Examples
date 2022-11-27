67c6511d47d623c2369ec2f2a32823a252832447
Dmitry <dmitry.sulyma@gmail.com>
2022-11-27 14:48:50 +0200

    Condition Notes Added

--- CondtitionExample/CConditionExample/Main.c
+++ CondtitionExample/CConditionExample/Main.c
@@ -2,6 +2,26 @@
 #define _UNCONDITIONAL_GOTO_H
 //#define _CONDITIONAL_GOTO_H
 
+/*
+
+Логічні значення в старих версіях 
+мови програмування С
+
+За допомогою констант:
+
+#define FALSE 0
+#define TRUE 1
+
+За допомогою перерахувань
+
+typedef enum
+{
+	false = 0,
+	true,
+} bool;
+
+*/
+
 #ifdef _UNCONDITIONAL_GOTO_H
 #include"UnconditionalGoto.c"
 #else
